2d0a69d74fa7b6daa92fd6e460f39b95682b439e
Yin Jun <yinjunok@foxmail.com>
2019-02-25 16:34:45 +0800

    actions 不能使用

--- start_test_demo/action-sequence.js
+++ start_test_demo/action-sequence.js
@@ -1,17 +1,15 @@
 const { Builder, By } = require('selenium-webdriver');
 
+// 貌似有 bug
+// https://github.com/SeleniumHQ/selenium/issues/5428
 (async function() {
   const dr = await new Builder().forBrowser('chrome').build();
   try {
     await dr.get('http://127.0.0.1:4000/action-sequence');
     const demo = await dr.findElement(By.id('demo'));
-    const body = await dr.findElement(By.tagName('body'));
-    dr.actions()
-      .mouseDown(demo)
-      .mouseMove(body, { x: 300, y: 500 })
-      .mouseUp(body)
-      .perform();
-
+    // const body = await dr.findElement(By.tagName('body'));
+    demo.click();
+    // dr.actions().click(demo).click(demo).perform();  
   } finally {
     setTimeout(() => {
       dr.quit();
